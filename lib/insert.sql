ef65af3baa1ca309b95ea873231c3e311d77f7d1
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-28 03:38:50 +0300

    complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 2, "M", "Black", "gentle", "TRUE" );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 3, "M", "Brown", "playful", "FALSE" );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 1, "F", "Grey", "social", "TRUE" );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 5, "M", "Brown", "fearful", "TRUE" );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 2, "F", "Blue-gray", "empathetic", "TRUE" );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 10, "M", "Cinnamon", "playful", "FALSE" );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 4, "F", "White", "joyful", "TRUE" );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 3, "F", "White", "joyful", "FALSE" );
+
